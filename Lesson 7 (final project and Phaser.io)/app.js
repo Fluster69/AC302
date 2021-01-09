b5c692223fd413e6c7a1e1249a8c1abea76f439f
ertreerr <miguel26wang@gmail.com>
2021-01-09 10:51:10 +0800

    Controls

--- Lesson 7 (final project and Phaser.io)/app.js
+++ Lesson 7 (final project and Phaser.io)/app.js
@@ -55,8 +55,57 @@ function create(){
 	lifetext = game.add.text(120, 5, life, style)
 	lifelabel.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 2)
 	lifetext.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 2)
+
+	player = game.add.sprite(32, 400, "onionheadweirdo");
+	player.animations.add("left", [0, 1, 2, 3], 10, true);
+	player.animations.add("right", [5, 6, 7, 8], 10, true);
+	game.physics.arcade.enable(player);
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
+
+	enemy1 = game.add.sprite(760, 20, "baddie");
+	player.animations.add("left", [0, 1], 10, true);
+	player.animations.add("right", [2, 3], 10, true);
+	game.physics.arcade.enable(player);
+	enemy1.body.bounce.y = 0.2;
+	enemy1.body.gravity.y = 500;
+	enemy1.body.collideWorldBounds = true;
+
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	for (var i =0; i < 12. length; i++) {
+		var star = stars.create(i * 70, 0, "star");
+		star.body.gravity.y = 200;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+	}
+
+	cusrors = game.input.keyboard.createCursorKeys()
+	wKey - game.input.keyboard.addKey(Phaser.keyboard.W)
+	aKey - game.input.keyboard.addKey(Phaser.keyboard.A)
+	dKey - game.input.keyboard.addKey(Phaser.keyboard.D)
 }
 
 function update(){
+	game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.collide(enemy1, platforms);
 
+	player.body.velocity.x = 0;
+
+	if(aKey.isDown || cusors.left.isDown){
+		player.body.velocity.x = -150;
+		player.animation.play("left");
+	}
+	else if(dKey.isDown || cursors.right.isDown){
+		player.body.velocity.x = -150;
+		player.animation.play("right");
+	}
+	else{
+		player.animations.stop();
+	}
+
+	if((wKey.isDown || cursors.up.isDown) && player.body.touching.down){
+		player.body.velocity.y = -300;
+	}
 }
